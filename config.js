0cd86fbffcdd9d179a00f374e2f143c5c6b0eab4
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-03-06 23:26:02 +0530

    fix: db connection string

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  DB_URL: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PSWD}@cluster0.2exvi.mongodb.net/polrr:27017?retryWrites=true&w=majority`,
+  DB_URL: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PSWD}@cluster0.2exvi.mongodb.net/polrr?retryWrites=true&w=majority`,
 }
